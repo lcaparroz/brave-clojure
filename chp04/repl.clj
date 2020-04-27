5fce915db088ac413242dc80f03b44f7a753315f
Luis Caparroz <lcaparroz.dev@gmail.com>
2020-04-26 21:12:34 -0300

    Add chp04 FWPD project

--- chp04/repl.clj
+++ chp04/repl.clj
@@ -125,3 +125,174 @@
 ;; concat
 
 (concat [1 2] [3 4])
+
+;; Lazy Seqs
+
+(def vampire-database
+  {0 {:makes-blood-puns? false :has-pulse? true  :name "McFishwich"}
+   1 {:makes-blood-puns? false :has-pulse? true  :name "McMackson"}
+   2 {:makes-blood-puns? true  :has-pulse? false :name "Damon Salvatore"}
+   3 {:makes-blood-puns? true  :has-pulse? true  :name "Mickey Mouse"}})
+
+(defn vampire-related-details
+  [social-security-number]
+  (Thread/sleep 1000)
+  (get vampire-database social-security-number))
+
+(defn vampire?
+  [record]
+  (and (:makes-blood-puns? record)
+       (not (:has-pulse? record))
+       record))
+
+(defn identify-vampire
+  [social-security-numbers]
+  (first (filter vampire?
+                 (map vampire-related-details social-security-numbers))))
+
+(time (vampire-related-details 0))
+(time (def mapped-details (map vampire-related-details (range 0 1000000))))
+(time (first mapped-details))
+(time (identify-vampire (range 0 1000000)))
+
+;; Infinite Sequences
+
+(concat (take 8 (repeat "na")) ["Batman!"])
+(take 3 (repeatedly (fn [] (rand-int 10))))
+
+(defn even-numbers
+  ([] (even-numbers 0))
+  ([n] (cons n (lazy-seq (even-numbers (+ n 2))))))
+
+(take 10 (even-numbers))
+(take 10 (even-numbers 10))
+(take 10 (even-numbers -18))
+
+(cons 0 '(2 4 6))
+
+;; The Collection Abstraction
+
+(empty? [])
+(empty? ["no!"])
+
+;; into
+;; adds one collection to another
+
+;; converting a mapped seq back to the original collection type
+
+(map identity {:sunlight-reaction "Glitter!"})
+(into {} (map identity {:sunlight-reaction "Glitter!"}))
+
+(map identity [:garlic :sesame-oil :fried-eggs])
+(into [] identity [:garlic :sesame-oil :fried-eggs])
+
+(map identity [:garlic-clove :garlic-clove])
+(into #{} (map identity [:garlic-clove :garlic-clove]))
+
+;; adding elements to non-empty collections
+
+(into {:favorite-emotion "gloomy"} [[:sunlight-reaction "Glitter!"]])
+(into ["cherry"] '("pine" "spruce"))
+
+(into {:favorite-animal "kitty"} {:least-favorite-smell "dog"
+                                  :relationship-with-teenager "creepy"})
+
+;; conj
+;; adds a scalar to a collection
+
+(conj [0] [1])
+(into [0] [1])
+
+(conj [0] 1)
+
+(conj [0] 1 2 3 4)
+(conj {:time "midnight"} [:place "ye olde cemetarium"])
+
+;; defining conj in terms of into
+
+(defn my-conj
+  [target & additions]
+  (into target additions))
+
+(my-conj [0] 1 2 3 4)
+
+;; Function functions
+
+;; apply
+;; it explodes seqable data, so it can be passed as multiple
+;; arguments to a function
+
+(max 0 1 2)
+(max [0 1 2])
+(apply max [0 1 2])
+
+;; defining into in terms of conj, by using apply
+;; notice that the function doesn't expect a rest parameter
+;; (param & rest)
+
+(defn my-into
+  [target additions]
+  (apply conj target additions))
+
+(my-into [0] [1 2 3 4])
+
+;; partial
+;; returns a function with "partially" applied arguments, it is, if you have a
+;; function which expects two arguments and pass it to partial along with an
+;; argument, partial will return a new function which expects just a second
+;; argument
+
+(def add10 (partial + 10))
+(add10 3)
+(add10 5)
+
+(def add-missing-elements
+  (partial conj ["water" "earth" "air"]))
+(add-missing-elements "unobtainium" "adamantium")
+
+;; defining partial
+
+(defn my-partial
+  [partialized-fn & args]
+  (fn [& more-args]
+    (apply partialized-fn (into args more-args))))
+
+(def add20 (my-partial + 20))
+(add20 3)
+
+(defn lousy-logger
+  [log-level message]
+  (condp = log-level
+    :warn (clojure.string/lower-case message)
+    :emergency (clojure.string/upper-case message)))
+
+(def warn (partial lousy-logger :warn))
+
+(warn "Reg light ahead")
+(lousy-logger :warn "Red light ahead")
+
+;; complement
+
+(defn identify-humans
+  [social-security-numbers]
+  (filter #(not (vampire? %))
+          (map vampire-related-details social-security-numbers)))
+(map :name (identify-humans (range 0 4)))
+
+(def not-vampire? (complement vampire?))
+(defn identify-humans-compl
+  [social-security-numbers]
+  (filter not-vampire?
+          (map vampire-related-details social-security-numbers)))
+(map :name (identify-humans-compl (range 0 4)))
+
+;; implementing complement
+
+(defn my-complement
+  [fun]
+  (fn [& args]
+    (not (apply fun args))))
+
+(def my-pos? (complement neg?))
+(my-pos? 1)
+(my-pos? -1)
